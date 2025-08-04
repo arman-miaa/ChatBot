83066f26dcb431864f71527c19df9864ac1d5c37
Arman <mdarman365506@gmail.com>
2025-08-04 16:25:42 +0600

    banner moble responsive done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,40 +1,51 @@
 import { PiVideoFill } from "react-icons/pi";
 import phoneImg from "../assets/Flat iPhone.png";
 import unionImg from "../assets/Union.png"
+import { useState } from "react";
+import VideoModal from "./VideoModel";
 
 const Banner = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div
       id="home"
-      className="bg-black overflow-hidden min-h-screen flex items-center justify-center py-16"
+      className="bg-black overflow-hidden min-h-screen flex items-center justify-center py-4 md:py-16"
     >
       <div className="text-center text-white px-4">
-        <span className="inline-block bg-red-300 text-primaryColor text-xs font-semibold px-3 py-1 rounded-full mb-4">
+        <span className="inline-block bg-red-300 text-primaryColor text-[9px] md:text-xs md:font-semibold px-3 py-1 rounded-full mb-4">
           #1 COMBAT SPORTS TICKETING PLATFORM
         </span>
-        <h1 className="text-6xl font-bold leading-15 mb-6">
+        <h1 className=" text-3xl md:text-6xl font-bold md:leading-15 mb-3 md:mb-6">
           Automate <span className="text-red-500">Customer</span>
           <br />
-          <span className="text-red-500">Interactions</span> with Your AI
-          <br />
-          Chatbot
+          <span className="text-red-500">Interactions</span> with Your  <br className="hidden md:flex" />
+           AI Chatbot
         </h1>
-        <p className="text-lg mb-8 max-w-2xl mx-auto text-secondaryColor">
+        <p className="hidden md:flex  justify-center text-sm md:text-lg mb-8 max-w-2xl mx-auto text-secondaryColor">
           Pre-trained and customisable chatbots for smarter conversations.
           <br />
-          CTA: 'Try for Free' and 'See Demo' buttons with accompanying visuals.
+          'Try for Free' and 'See Demo' buttons with accompanying visuals.
         </p>
-        <div className="flex flex-col  relative  justify-center items-center gap-4">
+        {/* for small device */}
+        <p className=" md:hidden text-sm md:text-lg mb-6 md:mb-8 max-w-2xl mx-auto text-secondaryColor">
+          Pre-trained and customisable chatbots for smarter conversations.
+          
+          'Try for Free' and 'See Demo'
+        </p>
+        <div className="flex flex-col  relative  justify-center items-center gap-3 md:gap-4">
           {/* img */}
           <img
             src={unionImg}
             alt=""
-            className="absolute right-18 top-2 w-[130px] object-cover"
+            className="absolute right-8 top-4 md:right-18 md:top-2 w-[49px] md:w-[130px] object-cover"
           />
-          <button className="bg-red-500 hover:bg-red-600 text-white font-medium py-2 px-12 cursor-pointer  rounded-lg flex items-center justify-center">
+          <button className="bg-red-500 hover:bg-red-600 text-white font-medium py-2 px-3 md:px-12 cursor-pointer  rounded-lg flex items-center justify-center">
             Register for free <span className="ml-2 text-xl">&rarr;</span>
           </button>
-          <button className="bg-white cursor-pointer hover:bg-gray-100 text-gray-800 font-semibold py-2 px-14 rounded-lg flex items-center justify-center">
+          <button
+            onClick={() => setShowModal(true)}
+            className="bg-white cursor-pointer hover:bg-gray-100 text-gray-800 font-semibold py-[10px] px-5 md:px-14 rounded-lg flex items-center justify-center"
+          >
             <span>
               {" "}
               <PiVideoFill className="text-primaryColor w-8 h-6" />
@@ -44,7 +55,7 @@ const Banner = () => {
         </div>
 
         {/* Image added below buttons */}
-        <div className="mt-20 flex justify-center">
+        <div className="md:mt-20  flex justify-center">
           <div className="relative rounded-[60px]">
             {/* Shadow / glow effect */}
             <div
@@ -54,7 +65,7 @@ const Banner = () => {
         rounded-[100px] 
         bg-gradient-to-b from-red-900/70 via-red-800/60 to-red-700/40
         blur-[100px] opacity-70
-        scale-x-[2.5] scale-y-[1.5]
+        md:scale-x-[2.5] md:scale-y-[1.5]
       "
             ></div>
 
@@ -62,11 +73,12 @@ const Banner = () => {
             <img
               src={phoneImg}
               alt="Phone"
-              className="relative w-[450px] h-[960px] object-contain rounded-[60px]"
+              className="relative w-[290px] mt-6 md:mt-0 md:w-[450px] md:h-[960px] object-contain rounded-[60px]"
             />
           </div>
         </div>
       </div>
+      {showModal && <VideoModal onClose={() => setShowModal(false)} />}
     </div>
   );
 };
---
+++ src/components/VideoModel.jsx
@@ -0,0 +1,33 @@
+const VideoModal = ({ onClose }) => {
+  return (
+    <div className="fixed inset-0 top-[66px] bg-black/80 z-50 flex items-center justify-center">
+      <div className="w-full mx-8 md:mx-0 md:w-1/2 bg-black rounded-xl overflow-hidden shadow-lg flex flex-col items-center">
+        {/* YouTube Video */}
+        <div className="aspect-video w-full">
+          <iframe
+            className="w-full h-full"
+            src="https://www.youtube.com/embed/tmgwq7AtjMw?si=2WuGVyuRAzZs27Ms"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </div>
+
+        {/* Close Section */}
+        <div
+          onClick={onClose}
+          className="flex  items-center border border-gray-200 cursor-pointer  p-1 px-3 rounded-full gap-2"
+        >
+          <span className="text-white font-semibold ">Close</span>
+          <button className="text-orange-500 text-lg hover:text-orange-600 transition">
+            ✖
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoModal;
